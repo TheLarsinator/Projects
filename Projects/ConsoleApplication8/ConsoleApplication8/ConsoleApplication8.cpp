82703be83ee1b77e8d79907f12548c2d5f351753
TheLarsinator <TheLarsinatormodding@hotmail.com>
2016-04-19 13:25:39 +0200

    Øvinger

---
+++ Projects/ConsoleApplication8/ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,19 @@
+// ConsoleApplication8.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	int a = 0;
+	for (int i = 0; i < 1; i++) {
+		a = i + 2*i;
+	}
+
+	std::cout << "ferdig !!!! " << i;
+	system("pause");
+    return 0;
+}
+
